c246e2299eb29e0409ce47a0996b480017685efb
Kushal Patel <113399080+kushal5294@users.noreply.github.com>
2025-10-12 22:35:25 -0400

    fweq

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,13 +1,29 @@
-import { Footer } from "../components/elements/Footer";
-import { Navbar } from "../components/elements/Navbar";
 import { Banner } from "../components/elements/Banner";
+import { BookingForm } from "../components/forms/BookingForm";
+import { Container } from "../components/shared/Container";
+import { Paragraph } from "../components/shared/Paragraph";
+import { darkBlue } from "../utils/constants";
 
 export const Booking = () => {
   return (
     <>
-      <Navbar />
       <Banner text="Book An Appointment" />
-      <Footer />
+      <section id="services" className="mb-13 mt-15">
+        <Container className="space-y-10 md:space-y-12 -mt-4">
+          <div className="text-center mx-auto space-y-4">
+            <Paragraph>
+              <span style={{ color: darkBlue }} className="font-bold">
+                Brighton Rx Pharmacy
+              </span>{" "}
+              cares about you. If you have any questions, concerns, or
+              suggestions, we encourage you to send us a message using the form
+              provided. One of our representatives will get back to you as soon
+              as possible.
+            </Paragraph>
+          </div>
+        </Container>
+      </section>
+      <BookingForm />
     </>
   );
 };
