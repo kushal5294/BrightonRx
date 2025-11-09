3cda2b03d1e3c7673192d837184e020dfb1b449f
Kushal Patel <113399080+kushal5294@users.noreply.github.com>
2025-11-09 14:50:46 -0500

    navbar

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Container } from "../shared/Container";
 import logo from "/assets/logos/BrightonRxLogo.png";
 import { NavItem } from "../shared/NavItem";
-import { phone, email } from "../../utils/constants";
+import { phone, email, fax } from "../../utils/constants";
 import { darkBlue } from "../../utils/constants";
 
 export const navItems = [
@@ -123,45 +123,70 @@ export const Navbar = () => {
                   </svg>
                   <a href={`tel:${phone}`}>{phone}</a>
                 </div>
+                {/* Fax */}
+                <div className="flex items-center gap-2 px-3">
+                  <img
+                    src="/assets/logos/fax.svg"
+                    alt="Fax Icon"
+                    className="w-5 h-5 align-middle"
+                    style={{
+                      filter:
+                        "invert(14%) sepia(79%) saturate(3781%) hue-rotate(188deg) brightness(93%) contrast(82%)",
+                    }}
+                  />
+                  <a href={`tel:${fax}`}>{fax}</a>
+                </div>
               </div>
             </div>
           </div>
 
           {/* --- Contact Info for small/medium --- */}
           <div
-            className="w-full flex flex-row items-center justify-center lg:hidden text-base md:text-lg gap-2"
+            className="w-full flex flex-col items-center justify-center lg:hidden text-base md:text-lg gap-2"
             style={{ color: darkBlue }}
           >
+            <div className="flex flex-row items-center gap-2">
+              <div className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-phone w-5 h-5 align-middle"
+                >
+                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                </svg>
+                <a href={`tel:${phone}`}>{phone}</a>
+              </div>
+              <span className="mx-2">|</span>
+              <div className="flex items-center gap-2">
+                <img
+                  src="/assets/logos/fax.svg"
+                  alt="Fax Icon"
+                  className="w-5 h-5 align-middle"
+                  style={{
+                    filter:
+                      "invert(14%) sepia(79%) saturate(3781%) hue-rotate(188deg) brightness(93%) contrast(82%)",
+                  }}
+                />
+                <a href={`tel:${fax}`}>{fax}</a>
+              </div>
+            </div>
             <div className="flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 8 6"
                 fill="currentColor"
-                className="w-5 h-5 align-middle hidden md:inline-block"
+                className="w-5 h-5 align-middle"
               >
                 <path d="m0 0h8v6h-8zm.75 .75v4.5h6.5v-4.5zM0 0l4 3 4-3v1l-4 3-4-3z" />
               </svg>
               <a href={`mailto:${email}`}>{email}</a>
-            </div>
-
-            <span className="mx-2">|</span>
-
-            <div className="flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-phone w-5 h-5 align-middle hidden md:inline-block"
-              >
-                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-              </svg>
-              <a href={`tel:${phone}`}>{phone}</a>
             </div>
           </div>
         </nav>
