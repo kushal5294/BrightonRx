0209ff311b936fe1fe0e88cb89fa57341cac65ab
Kushal Patel <113399080+kushal5294@users.noreply.github.com>
2026-03-14 18:52:45 -0400

    new email format

--- src/utils/formSubmit.ts
+++ src/utils/formSubmit.ts
@@ -1,141 +1,46 @@
 import axios from "axios";
 import { useModalStore } from "../store/modalStore";
 
-const jsonToString = (data: any): string => {
-  const { formData, prescriptions, rxNumbers, transferAll } = data;
-  let content = "";
+const buildPayload = (formData: any) => {
+  const fd = formData.formData;
 
-  switch (formData.form) {
-    case "auto-refill":
-      content += `New Auto Refill Form Submission:\n\n`;
-      content += `First Name: ${formData.firstName}\n`;
-      content += `Last Name: ${formData.lastName}\n`;
-      content += `Email: ${formData.email}\n`;
-      content += `Phone: ${formData.phone}\n`;
-      content += `Auto-Refill: ${formData.autoRefill ? "Yes" : "No"}\n`;
-      content += `Notification: ${formData.notification}\n`;
-      break;
+  return {
+    // Basic info
+    firstName: fd.firstName || "",
+    lastName: fd.lastName || "",
+    email: fd.email || "",
+    phone: fd.phone || "",
+    address: fd.address || "",
+    dob: fd.dob || "",
+    fax: fd.fax || "",
+    formType: fd.form || "",
 
-    case "booking":
-      content += `New Booking Form Submission:\n\n`;
-      content += `First Name: ${formData.firstName}\n`;
-      content += `Last Name: ${formData.lastName}\n`;
-      content += `Service: ${formData.service}\n`;
-      content += `Phone: ${formData.phone}\n`;
-      content += `Date: ${formData.date}\n`;
-      content += `Time: ${formData.time}\n`;
-      if (formData.question) {
-        content += `Comments/Preferences: ${formData.question}\n`;
-      }
-      break;
+    // Form-specific fields
+    service: fd.service || "",
+    date: fd.date || "",
+    time: fd.time || "",
+    question: fd.question || "",
+    autoRefill: fd.autoRefill || false,
+    notification: fd.notification || "",
+    pickupMethod: fd.pickupMethod || "",
+    delivery: fd.delivery || false,
+    contactMethod: fd.contactMethod || "",
+    oldPharmacyName: fd.oldPharmacyName || "",
+    oldPharmacyPhone: fd.oldPharmacyPhone || "",
 
-    case "consult":
-      content += `New Consultation Form Submission:\n\n`;
-      content += `First Name: ${formData.firstName}\n`;
-      content += `Last Name: ${formData.lastName}\n`;
-      content += `Address: ${formData.address}\n`;
-      content += `Phone: ${formData.phone}\n`;
-      content += `Best Time to Call: ${formData.bestTimeToCall}\n`;
-      if (formData.email) {
-        content += `Email: ${formData.email}\n`;
-      }
-      if (formData.fax) {
-        content += `Fax: ${formData.fax}\n`;
-      }
-      content += `Preferred Contact Method: ${formData.contactMethod}\n`;
-      if (formData.question) {
-        content += `Comments/Preferences: ${formData.question}\n`;
-      }
-      break;
-
-    case "contact":
-      content += `New Contact Form Submission:\n\n`;
-      content += `First Name: ${formData.firstName}\n`;
-      content += `Last Name: ${formData.lastName}\n`;
-      content += `Address: ${formData.address}\n`;
-      content += `Email: ${formData.email}\n`;
-      content += `Phone: ${formData.phone}\n`;
-      if (formData.question) {
-        content += `Question/Comment: ${formData.question}\n`;
-      }
-      break;
-
-    case "delivery":
-      content += `New Delivery Form Submission:\n\n`;
-      content += `First Name: ${formData.firstName}\n`;
-      content += `Last Name: ${formData.lastName}\n`;
-      content += `Email: ${formData.email}\n`;
-      content += `Phone: ${formData.phone}\n`;
-      content += `Free Pick-up and Delivery: ${
-        formData.delivery ? "Yes" : "No"
-      }\n`;
-      content += `Notification: ${formData.notification}\n`;
-      break;
-
-    case "refill":
-      content += `New Prescription Refill Form Submission:\n\n`;
-      content += `First Name: ${formData.firstName}\n`;
-      content += `Last Name: ${formData.lastName}\n`;
-      content += `Phone: ${formData.phone}\n`;
-      content += `Pickup/Delivery: ${formData.pickupMethod}\n`;
-      content += `Notification: ${formData.notification}\n\n`;
-      if (rxNumbers && rxNumbers.length > 0) {
-        content += "RX Refill Numbers:\n";
-        rxNumbers.forEach((rx: string) => {
-          if (rx) content += `  - ${rx}\n`;
-        });
-        content += "\n";
-      }
-      if (prescriptions && prescriptions.length > 0) {
-        content += "Over the Counter Items:\n";
-        prescriptions.forEach((p: any) => {
-          if (p.name)
-            content += `  - Name: ${p.name}, Quantity: ${p.quantity}\n`;
-        });
-      }
-      break;
-
-    case "transfer":
-      content += `New Prescription Transfer Form Submission:\n\n`;
-      content += `First Name: ${formData.firstName}\n`;
-      content += `Last Name: ${formData.lastName}\n`;
-      content += `Phone: ${formData.phone}\n`;
-      content += `Date of Birth: ${formData.dob}\n`;
-      content += `Address: ${formData.address}\n`;
-      content += `Old Pharmacy Name: ${formData.oldPharmacyName}\n`;
-      content += `Old Pharmacy Phone: ${formData.oldPharmacyPhone}\n\n`;
-      if (transferAll) {
-        content += "Transfer all prescriptions.\n";
-      } else if (prescriptions && prescriptions.length > 0) {
-        content += "Prescriptions to Transfer:\n";
-        prescriptions.forEach((p: any) => {
-          if (p.name)
-            content += `  - Medication: ${p.name}, Rx #: ${p.RxNumber}\n`;
-        });
-      }
-      break;
-
-    default:
-      content += JSON.stringify(data, null, 2);
-  }
-
-  return content;
+    // Prescriptions / transfers
+    rxNumbers: formData.rxNumbers || [],
+    prescriptions: formData.prescriptions || [],
+    transferAll: formData.transferAll || false,
+  };
 };
 
-const sendEmail = async (data: any) => {
-  const emailContent = jsonToString(data);
-
-  const payload = {
-    firstName: data.formData.firstName,
-    lastName: data.formData.lastName,
-    message: emailContent,
-  };
+const sendEmail = async (formData: any) => {
+  const payload = buildPayload(formData);
 
   try {
     await axios.post("https://brightonrxpharmacy.com/api/send-email", payload, {
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
     });
 
     useModalStore.getState().openModal({
@@ -145,7 +50,7 @@ const sendEmail = async (data: any) => {
       status: "success",
     });
   } catch (error) {
-    console.error(error); // keep this for debugging
+    console.error(error);
     useModalStore.getState().openModal({
       title: "Submission Failed",
       message:
@@ -156,8 +61,6 @@ const sendEmail = async (data: any) => {
 };
 
 export const handleFormSubmit = async (formData: any, setIsLoading: any) => {
-  // console.log("Form submitted:", formData.formData.form);
-  // console.log(formData);
   setIsLoading(true);
   await sendEmail(formData);
   setIsLoading(false);
